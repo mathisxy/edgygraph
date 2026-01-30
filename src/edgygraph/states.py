48c953e8b9338587355916924771c89b0a19fac3
mathisxy <mathis.siebert@gmail.com>
2026-01-30 22:12:43 +0000

    reformatted

--- src/edgygraph/states.py
+++ src/edgygraph/states.py
@@ -5,6 +5,33 @@ from types import TracebackType
 from asyncio import Lock
 
 
+class State(BaseModel):
+    """
+    Holds variables for the nodes of serializable types. Arbitrary types are not allowed.
+
+    The state is copied for each parallel node and merged after the node has finished.
+    Therefore, the state is not shared between nodes and operations are safe.
+
+    Parallel changes of the same variable will be detected as a conflict and raise an error.
+
+    Implements pydantic's BaseModel.
+    """
+    model_config = ConfigDict(arbitrary_types_allowed=False) # for deep copy
+
+
+class Shared(BaseModel):
+    """
+    Holds shared variables for the nodes of any type.
+
+    The shared state is shared between all parallel nodes and operations are not safe without using the Lock.
+    The lock can be accessed via `shared.lock`.
+
+    Implements pydantic's BaseModel with arbitrary types allowed.
+    """
+    lock: Lock = Field(default_factory=Lock)
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+
 class Stream[T: object](ABC, AsyncIterator[T]):
     """
     Standardized wrapper interface for streams of data.
@@ -33,30 +60,3 @@ class Stream[T: object](ABC, AsyncIterator[T]):
         ) -> None: # Not handling exceptions here -> returns None
 
         await self.aclose()
-
-
-class State(BaseModel):
-    """
-    Holds variables for the nodes of serializable types. Arbitrary types are not allowed.
-
-    The state is copied for each parallel node and merged after the node has finished.
-    Therefore, the state is not shared between nodes and operations are safe.
-
-    Parallel changes of the same variable will be detected as a conflict and raise an error.
-
-    Implements pydantic's BaseModel.
-    """
-    model_config = ConfigDict(arbitrary_types_allowed=False) # for deep copy
-
-
-class Shared(BaseModel):
-    """
-    Holds shared variables for the nodes of any type.
-
-    The shared state is shared between all parallel nodes and operations are not safe without using the Lock.
-    The lock can be accessed via `shared.lock`.
-
-    Implements pydantic's BaseModel with arbitrary types allowed.
-    """
-    lock: Lock = Field(default_factory=Lock)
-    model_config = ConfigDict(arbitrary_types_allowed=True)
