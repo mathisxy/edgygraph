d0a3b827150284941a6e0ec2028219989d02eb88
mathisxy <mathis.siebert@gmail.com>
2026-01-31 22:16:32 +0000

    added hello world example

---
+++ examples/hello_world.py
@@ -0,0 +1,50 @@
+from edgygraph import State, Shared, Node, START, END, Graph
+import asyncio
+
+
+### STATES
+
+class MyState(State):
+
+    capslock: bool = False
+
+
+### NODES
+
+class MyNode(Node[MyState, Shared]):
+
+    async def run(self, state: MyState, shared: Shared) -> None:
+
+        if state.capslock:
+            print("HELLO WORLD!")
+        else:
+            print("Hello World!")
+
+
+### INSTANCES
+
+state = MyState(capslock=True)
+shared = Shared()
+
+node = MyNode()
+
+
+### GRAPH
+
+graph = Graph[MyState, Shared](
+    edges=[
+        (
+            START,
+            node
+        ),
+        (
+            node,
+            END
+        )
+    ]
+)
+
+
+### RUN
+
+asyncio.run(graph(state, shared))
